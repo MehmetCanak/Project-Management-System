93c793680150a398285f1e57a4716759cae1a852
mehmet <mehmet.canak.36@gmail.com>
2022-03-02 17:25:28 +0300

    Add new component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './project/login/login.component';
+import { DashboardComponent } from './project/dashboard/dashboard.component';
+import { ComponentsComponent } from './project/components/components.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ComponentsComponent
   ],
   imports: [
     BrowserModule,
